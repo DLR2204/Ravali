e2e370cd13ea583ad4ebbc80002af4026d04d7b5
DLR2204 <n130488@rguktn.ac.in>
2024-07-12 18:52:17 +0530

    Add files via upload

--- SpringBoot/com/controller/Controller.java
+++ SpringBoot/com/controller/Controller.java
@@ -1,5 +1,40 @@
 package com.controller;
 
-public class Controller {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.entity.Employee;
+import com.service.Services;
+
+@RestController
+@RequestMapping("/emp")
+public class Controller {
+	
+	@Autowired
+	Services service;
+	
+	@PostMapping("/addEmp")//http://localhost:1801/emp/addEmp
+	public String insertEmp(@RequestBody Employee emp) {
+		return service.addEmp(emp);
+	}
+	@PutMapping("/updateEmp")//http/localhost:1801/emp/addEmp
+	public String updateEmp(@RequestBody Employee emp) {
+		return service.updateEmp(emp);
+	}
+
+	@DeleteMapping("/deleteEmp/{eid}")//http/localhost:1801/emp/addEmp
+	public String deleteEmp(@PathVariable("eid") int empId  ) {
+		return service.deleteEmp(empId);
+	}
+	@GetMapping("/getEmp/{eid}")//http/localhost:1801/emp/addEmp
+	public Employee getEmp(@PathVariable("eid") int empId) {
+		return service.getEmp(empId);
+	}
 }
